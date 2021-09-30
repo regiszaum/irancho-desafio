9043826f0885538be34fc88f6138430c359a3ca2
regisfilho <regisfilho@pactosolucoes.com.br>
2021-09-29 23:54:12 -0300

    first commit

---
+++ backend/src/config/config.js
@@ -0,0 +1,8 @@
+//configuaração da cominicação com o banco de dados 
+module.exports = {
+  username: 'root',
+  password: 'root',
+  database: 'irancho_api',
+  host: '127.0.0.1',
+  dialect: 'mysql',
+}
